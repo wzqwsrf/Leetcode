1f4f253a5ed967a273c53a4c52256f381210bc72
zhenqing.wang <zhenqing.wang@qunar.com>
2014-07-03 11:58:43 +0800

    add spiral matrix II

--- Java/leetcode.java
+++ Java/leetcode.java
@@ -67,9 +67,9 @@ class Solution {
 /*
  * 1、借助Map，每次边检查边存数据。一直到找到结果为止。
  * 2、疑问，例如numbers = [2,2,3,7,6], target = 5
- *    用了两种方法，得出的结果一个为[1,3]，另一个为[2,3]，
- *    居然都AC了。
- *    具体参考：http://blog.csdn.net/u013027996/article/details/17660515
+ * 用了两种方法，得出的结果一个为[1,3]，另一个为[2,3]，
+ * 居然都AC了。
+ * 具体参考：http://blog.csdn.net/u013027996/article/details/17660515
  */
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
@@ -135,5 +135,50 @@ public class Solution {
 			endy--;
 		}
 		return allList;
+	}
+}
+
+//Spiral Matrix II 348ms
+/*
+ * 和Spiral Matrix一样，但是这次不是打印矩阵的内容了，而是赋值，思路仍然一致。
+ * 1、设定边界值，startx，endx，starty，endy。
+ * 2、左到右，上到下，右到左，下到上，直到while条件结束。
+ * 具体参考http://blog.csdn.net/u013027996/article/details/36638519
+ */
+public class Solution {
+    public int[][] generateMatrix(int n) {
+		int [][]matrix = new int[n][n];
+		int startx = 0;
+		int starty = 0;
+		int endx = n - 1;
+		int endy = n - 1;
+		int num = 1;
+		while (startx <= endx && starty <= endy) {
+			for (int i = starty; i <= endy; i++) {
+				matrix[startx][i] = num;
+				num++;
+			}
+			for (int i = startx+1; i <= endx; i++) {
+				matrix[i][endy] = num;
+				num++;
+			}
+			if (startx != endx) {
+				for (int i = endy-1; i >= starty; i--) {
+					matrix[endx][i] = num;
+					num++;
+				}
+			}
+			if (starty != endy) {
+				for (int i = endx-1; i > startx; i--) {
+					matrix[i][starty] = num;
+					num++;
+				}
+			}
+			startx++;
+			endx--;
+			starty++;
+			endy--;
+		}
+		return matrix;
 	}
 }
