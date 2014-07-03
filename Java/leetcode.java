21829d2ac1f5fa5df4aaa117680192736d742a30
zhenqing.wang <zhenqing.wang@qunar.com>
2014-07-03 14:51:36 +0800

    add url

--- Java/leetcode.java
+++ Java/leetcode.java
@@ -1,5 +1,9 @@
 // Add Binary 444ms
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日17:25:17
+ * @url:https://oj.leetcode.com/problems/add-binary/
  * 1、二进制加法，最基本的还是将字符串转为字符数组，按位相加，注意进位问题。
  * 2、两个数相加的最大位数，是二者长度之中最大值或者最大值加1
  * 具体参考：http://blog.csdn.net/u013027996/article/details/36436901
@@ -65,6 +69,10 @@ class Solution {
 
 // Two Sum 384ms
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日18:05:21
+ * @url:https://oj.leetcode.com/problems/two-sum/
  * 1、借助Map，每次边检查边存数据。一直到找到结果为止。
  * 2、疑问，例如numbers = [2,2,3,7,6], target = 5
  * 用了两种方法，得出的结果一个为[1,3]，另一个为[2,3]，
@@ -90,6 +98,10 @@ public class Solution {
 
 // Spiral Matrix 340ms
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日18:56:01
+ * @url:https://oj.leetcode.com/problems/spiral-matrix/
  * 1、设定边界值，startx，endx，starty，endy。
  * 2、左到右，上到下，右到左，下到上，直到while条件结束。
  * 具体参考http://blog.csdn.net/u013027996/article/details/36631355
@@ -140,6 +152,10 @@ public class Solution {
 
 //Spiral Matrix II 348ms
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月03日109:51:32
+ * @url:https://oj.leetcode.com/problems/spiral-matrix-ii/
  * 和Spiral Matrix一样，但是这次不是打印矩阵的内容了，而是赋值，思路仍然一致。
  * 1、设定边界值，startx，endx，starty，endy。
  * 2、左到右，上到下，右到左，下到上，直到while条件结束。
--- Python/leetcode.py
+++ Python/leetcode.py
@@ -1,6 +1,10 @@
 # Add Binary
 '''
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日17:25:17
+ * @url:https://oj.leetcode.com/problems/add-binary/
  * 1、二进制加法，最基本的还是将字符串转为字符数组，按位相加，注意进位问题。
  * 2、两个数相加的最大位数，是二者长度之中最大值或者最大值加1
  * 具体参考：http://blog.csdn.net/u013027996/article/details/36436901
@@ -50,11 +54,15 @@ class Solution:
 # Two Sum 172ms
 '''
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日18:05:21
+ * @url:https://oj.leetcode.com/problems/two-sum/
  * 1、借助Map，每次边检查边存数据。一直到找到结果为止。
  * 2、疑问，例如numbers = [2,2,3,7,6], target = 5
- *    用了两种方法，得出的结果一个为[1,3]，另一个为[2,3]，
- *    居然都AC了。
- *    具体参考：http://blog.csdn.net/u013027996/article/details/17660515
+ * 用了两种方法，得出的结果一个为[1,3]，另一个为[2,3]，
+ * 居然都AC了。
+ * 具体参考：http://blog.csdn.net/u013027996/article/details/17660515
  */
 '''
 class Solution:
@@ -74,6 +82,10 @@ class Solution:
 # Spiral Matrix 76ms
 '''
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月02日18:56:01
+ * @url:https://oj.leetcode.com/problems/spiral-matrix/
  * 1、设定边界值，startx，endx，starty，endy。
  * 2、左到右，上到下，右到左，下到上，直到while条件结束。
  * 具体参考http://blog.csdn.net/u013027996/article/details/36631355
@@ -112,6 +124,10 @@ class Solution:
 # Spiral Matrix II 208ms
 '''
 /*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年07月03日109:51:32
+ * @url:https://oj.leetcode.com/problems/spiral-matrix-ii/
  * 和Spiral Matrix一样，但是这次不是打印矩阵的内容了，而是赋值，思路仍然一致。
  * 1、设定边界值，startx，endx，starty，endy。
  * 2、左到右，上到下，右到左，下到上，直到while条件结束。
