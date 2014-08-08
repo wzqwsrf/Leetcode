47bea767852cb811663c7a5fd81ff9801a806564
zhenqing.wang <zhenqing.wang@qunar.com>
2014-08-08 19:02:37 +0800

    add Maximum Subarray

--- Java/leetcode.java
+++ Java/leetcode.java
@@ -637,3 +637,36 @@ public class Solution {
     }
 }
 
+
+
+//======================================
+// Maximum Subarray 408ms
+/*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年08月08日19:00:34
+ * @url:https://oj.leetcode.com/problems/maximum-subarray/
+ * 最大连续子数组和，非常经典
+ * 具体参考http://blog.csdn.net/u013027996/article/details/38423167
+ */
+public class Solution {
+    public int maxSubArray(int[] A) {
+        int len = A.length;
+        if(len == 1){
+            return A[0];
+        }
+        int maxSum = A[0];
+        int tempSum = 0;
+        for(int i = 0; i < len ; i++){
+            if(tempSum < 0){
+                tempSum = A[i];
+            }else if(tempSum >= 0){
+                tempSum += A[i];
+            }
+            if(tempSum > maxSum){
+                maxSum = tempSum;
+            }
+        }
+        return maxSum;
+    }
+}
--- Python/leetcode.py
+++ Python/leetcode.py
@@ -547,3 +547,33 @@ class Solution:
         return array[n]
 
 
+
+#======================================
+# Maximum Subarray 300ms
+'''
+/*
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2014年08月08日19:00:34
+ * @url:https://oj.leetcode.com/problems/maximum-subarray/
+ * 最大连续子数组和，非常经典
+ * 具体参考http://blog.csdn.net/u013027996/article/details/38423167
+ */
+ '''
+class Solution:
+    # @param A, a list of integers
+    # @return an integer
+    def maxSubArray(self, A):
+        if A is None or len(A) == 0:
+            return 0
+        a_len = len(A)
+        maxSum = A[0]
+        tempSum = 0
+        for i in range(a_len):
+            if tempSum < 0:
+                tempSum = A[i]
+            else:
+                tempSum += A[i]
+            if tempSum > maxSum:
+                maxSum = tempSum
+        return maxSum
