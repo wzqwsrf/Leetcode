f679963bcaa8193bd134220b727ad1adcb57f0ce
zhenqing.wang <zhenqing.wang@qunar.com>
2014-10-17 11:41:21 +0800

    add Maximum Product Subarray

--- Java/leetcode.java
+++ Java/leetcode.java
@@ -781,3 +781,34 @@ public class Solution {
         return start;
     }
 }
+
+
+
+//======================================
+//Maximum Product Subarray 428ms
+/*
+* @author:wangzq
+* @email:wangzhenqing1008@163.com
+* @date:2014年10月17日11:39:35
+* @url:https://oj.leetcode.com/problems/maximum-product-subarray/
+* 最大连续字数组乘积。
+* 具体参考http://blog.csdn.net/u013027996/article/details/40183455
+*/
+public class Solution {
+    public int maxProduct(int[] A) {
+        int len = A.length;
+        int max = A[0];
+        int min = A[0];
+        int temp1 = 0;
+        int temp2 = 0;
+        int result = A[0];
+        for(int i = 1; i < len; i++){
+            temp1 = min * A[i];
+            temp2 = max * A[i];
+            min = Math.min(Math.min(temp1, temp2), A[i]);
+            max = Math.max(Math.max(temp1, temp2), A[i]);
+            result = max > result ? max : result;
+        }
+        return result;
+    }
+}
