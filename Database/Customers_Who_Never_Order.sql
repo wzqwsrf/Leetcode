ba7480c0ef802068885749b95bdcddcf92aa0fcc
wangzhenqing <wangzhenqing1008@163.com>
2015-01-22 14:38:00 +0800

    add 183,184

---
+++ Database/Customers_Who_Never_Order.sql
@@ -0,0 +1,35 @@
+Customers Who Never Order
+Total Accepted: 226 Total Submissions: 639 My Submissions Question Solution 
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+MySQL:
+
+# Write your MySQL query statement below
+select name Customers from Customers where id not in (select CustomerId from Orders);
