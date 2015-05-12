61df1fd42a38e9c502b4191556a9dbcd1dcfb9f3
wangzhenqing <wzqwsrf@126.com>
2015-05-12 17:40:44 +0800

    add 196 197

---
+++ Database/Delete_Duplicate_Emails.sql
@@ -0,0 +1,25 @@
+Delete Duplicate Emails
+Total Accepted: 1825 Total Submissions: 9799 My Submissions Question Solution
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+
+MySQL:1191ms
+
+# Write your MySQL query statement below
+delete from Person where Id not in (select t.Id from (select min(Id) Id from Person group by Email) t);
