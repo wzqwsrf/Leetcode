097d33f5196e0ef136b4b119517a49ee2a9ba2d6
wangzhenqing <wzqwsrf@126.com>
2015-06-20 15:55:04 +0800

    add 126 127

---
+++ Java/Word_Ladder.java
@@ -0,0 +1,59 @@
+
+//Word Ladder
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2015年06月20日15:50:54
+ * @url:https://leetcode.com/problems/word-ladder/
+ */
+
+public class Word_Ladder {
+    public static int ladderLength(String start, String end,
+                                   HashSet<String> dict) {
+        if (dict == null || dict.isEmpty()) {
+            return 0;
+        }
+        Queue<Node> strQueue = new LinkedList<Node>();
+        HashSet<String> visitedSet = new HashSet<String>();
+        strQueue.add(new Node(start, 0));
+        visitedSet.add(start);
+        int wordLen = start.length();
+        while (!strQueue.isEmpty()) {
+            Node node = strQueue.peek();
+            strQueue.poll();
+            for (int i = 0; i < wordLen; i++) {
+                StringBuffer sb = new StringBuffer(node.word);
+                for (char j = 'a'; j <= 'z'; j++) {
+                    sb.setCharAt(i, j);
+                    String tempWord = sb.toString();
+                    if (dict.contains(tempWord) &&
+                            !visitedSet.contains(tempWord)) {
+                        if (tempWord.equals(end)) {
+                            return node.len + 2;
+                        } else {
+                            strQueue.add(new Node(tempWord, node.len + 1));
+                            visitedSet.add(tempWord);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static class Node {
+        String word;
+        int len;
+
+        public Node(String word, int len) {
+            super();
+            this.word = word;
+            this.len = len;
+        }
+    }
+}
---
+++ Java/Word_Ladder_II.java
@@ -0,0 +1,98 @@
+
+//Word Ladder II
+
+import java.util.*;
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2015年06月20日15:52:44
+ * @url:https://leetcode.com/problems/word-ladder-ii/
+ */
+
+public class Word_Ladder_II {
+
+    private ArrayList<ArrayList<String>> list = new ArrayList<ArrayList<String>>();
+
+    private Map<String, List<String>> map = new HashMap<String, List<String>>();
+
+    public ArrayList<ArrayList<String>> findLadders(String start,
+                                                    String end,
+                                                    HashSet<String> dict) {
+
+        if (dict == null || dict.isEmpty()) {
+            return list;
+        }
+
+        Queue<String> strQueue = new LinkedList<String>();
+        strQueue.add(start);
+        Map<String, Integer> levelMap = new HashMap<String, Integer>();
+        levelMap.put(start, 0);
+        int wordLen = start.length();
+        boolean found = false;
+        while (!strQueue.isEmpty() && !found) {
+            int size = strQueue.size();
+            while (size > 0) {
+                size--;
+                String newWord = strQueue.poll();
+                int len = levelMap.get(newWord);
+                for (int i = 0; i < wordLen; i++) {
+                    StringBuilder sb = new StringBuilder(newWord);
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        sb.setCharAt(i, j);
+                        String tempWord = sb.toString();
+                        if (tempWord.equals(end)) {
+                            found = true;
+                            constructMap(tempWord, newWord);
+                        } else if (!found && dict.contains(tempWord)) {
+                            if (!levelMap.containsKey(tempWord)) {
+                                levelMap.put(tempWord, len + 1);
+                                strQueue.add(tempWord);
+                                constructMap(tempWord, newWord);
+                            } else if (levelMap.containsKey(tempWord)
+                                    && levelMap.get(tempWord) == len + 1) {
+                                constructMap(tempWord, newWord);
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+        if (map.size() == 0) {
+            return list;
+        }
+        List<String> path = new ArrayList<String>();
+        path.add(end);
+        dfs(end, path);
+        return list;
+    }
+
+    private void constructMap(String curWord, String lastWord) {
+        List<String> preList = map.get(curWord);
+        if (preList == null) {
+            preList = new ArrayList<String>();
+        }
+        if (!preList.contains(lastWord)) {
+            preList.add(lastWord);
+        }
+        map.put(curWord, preList);
+    }
+
+    private void dfs(String end, List<String> path) {
+        List<String> preList = map.get(end);
+        if (preList == null) {
+            ArrayList<String> newList = new ArrayList<String>();
+            for (int i = path.size() - 1; i >= 0; i--) {
+                newList.add(path.get(i));
+            }
+            list.add(newList);
+        } else {
+            for (String s : preList) {
+                path.add(s);
+                dfs(s, path);
+                path.remove(s);
+            }
+        }
+    }
+}
