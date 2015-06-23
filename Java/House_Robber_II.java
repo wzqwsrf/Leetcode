a591639f978242b65979ee80e9c56c3a05ec4895
wangzhenqing <wzqwsrf@126.com>
2015-06-23 17:58:26 +0800

    add 213

---
+++ Java/House_Robber_II.java
@@ -0,0 +1,37 @@
+
+//House Robber II
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2015年06月23日17:57:17
+ * @url:https://leetcode.com/problems/house-robber-ii/
+ */
+
+public class House_Robber_II {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int dp1[] = new int[len];
+        dp1[0] = nums[0];
+        dp1[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < len - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + nums[i]);
+        }
+        int dp2[] = new int[len];
+        dp2[0] = 0;
+        dp2[1] = nums[1];
+        for (int i = 2; i < len; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + nums[i]);
+        }
+        return Math.max(dp1[len - 2], dp2[len - 1]);
+    }
+}
