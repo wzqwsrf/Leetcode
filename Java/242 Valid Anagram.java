2ab0f37a4d8df5dabe035010fce6714561725fe6
wangzhenqing <wzqwsrf@126.com>
2015-08-03 10:40:29 +0800

    add 242

---
+++ Java/242 Valid Anagram.java
@@ -0,0 +1,37 @@
+
+// Valid Anagram
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2015-08-03 10:39:42
+ * @url:https://leetcode.com/problems/valid-anagram/
+ */
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        int sLen = s.length();
+        int tLen = t.length();
+        if (sLen != tLen) {
+            return false;
+        }
+        char sArray[] = s.toCharArray();
+        char tArray[] = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        int k = 0;
+        while (k < sLen) {
+            if (sArray[k] != tArray[k]) {
+                return false;
+            }
+            k++;
+        }
+        return true;
+    }
+}
