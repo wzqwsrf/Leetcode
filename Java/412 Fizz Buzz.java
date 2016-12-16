e8d99605cc8c7ac1114c006f14d392460e263ce1
wangzhenqing <wzqwsrf@126.com>
2016-12-16 21:25:07 +0800

    add 412

---
+++ Java/412 Fizz Buzz.java
@@ -0,0 +1,27 @@
+
+// 412. Fizz Buzz
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2016-12-16 21:24:42
+ * @url:https://leetcode.com/problems/fizz-buzz/
+ */
+
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+        return list;
+    }
+}
