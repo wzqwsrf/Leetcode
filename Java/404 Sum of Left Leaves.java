146eb60f24c95c68bffe3c8633aba6013502c6d5
wangzhenqing <wzqwsrf@126.com>
2017-01-06 16:19:08 +0800

    add 404

---
+++ Java/404 Sum of Left Leaves.java
@@ -0,0 +1,36 @@
+
+// 404. Sum of Left Leaves
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2017-1-6 16:18:13
+ * @url:https://leetcode.com/problems/sum-of-left-leaves/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
