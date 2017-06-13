2a0808b2756feea8a05f50d6cafaa0d2b19cc88b
wangzhenqing <wzqwsrf@126.com>
2017-06-13 19:15:38 +0800

    add 617

---
+++ Java/617 Merge Two Binary Trees.java
@@ -0,0 +1,33 @@
+
+// 617. Merge Two Binary Trees
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2017-6-13 19:14:37
+ * @url:https://leetcode.com/problems/merge-two-binary-trees/#/description
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+}
