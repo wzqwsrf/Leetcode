ba52160999fc3c2f407683ebd1c9e7642c756369
wangzhenqing <wzqwsrf@126.com>
2017-06-17 13:32:25 +0800

    add 596

---
+++ Database/596 Classes More Than 5 Students.sql
@@ -0,0 +1,34 @@
+596. Classes More Than 5 Students
+
+There is a table courses with columns: student and class
+
+Please list out all classes which have more than or equal to 5 students.
+
+For example, the table:
+
++---------+------------+
+| student | class      |
++---------+------------+
+| A       | Math       |
+| B       | English    |
+| C       | Math       |
+| D       | Biology    |
+| E       | Math       |
+| F       | Computer   |
+| G       | Math       |
+| H       | Math       |
+| I       | Math       |
++---------+------------+
+Should output:
+
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Note:
+The students should not be counted duplicate in each course.
+
+# Write your MySQL query statement below
+select class from
+(select class, count(distinct student) stu from courses c group by class) t where t.stu >= 5;
