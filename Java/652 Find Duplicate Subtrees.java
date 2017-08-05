b6507cf0afea48e601eb85b25af22488d58607ed
liuyazhou <wzqwsrf@126.com>
2017-08-05 16:23:46 +0800

    652

--- Java/652 Find Duplicate Subtrees.java
+++ Java/652 Find Duplicate Subtrees.java
@@ -18,27 +18,28 @@
  * }
  */
 public class Solution {
-    private List<TreeNode> list;
-    private Map<String, Integer> map;
-
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        list = new ArrayList<TreeNode>();
-        map = new HashMap<String, Integer>();
-        dfs(root);
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        dfs(root, map, list);
         return list;
     }
 
-    private String dfs(TreeNode root) {
+    private String dfs(TreeNode root, Map<String, Integer> map, List<TreeNode> list) {
         if (root == null) {
             return "null";
         }
         String key = String.valueOf(root.val) + "," +
-                dfs(root.left) + "," + dfs(root.right);
-        Integer num = map.getOrDefault(key, 0);
-        if (num == 1) {
-            list.add(root);
+                dfs(root.left, map, list) + "," + dfs(root.right, map, list);
+        Integer num = map.get(key);
+        if (num != null) {
+            if (num == 1) {
+                list.add(root);
+            }
+            num += 1;
+        } else {
+            num = 1;
         }
-        num += 1;
         map.put(key, num);
         return key;
     }
