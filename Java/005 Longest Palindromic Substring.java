639eaf7d932a88f7869a0b6c34de632fe15ad9ed
liuyazhou <wzqwsrf@126.com>
2017-08-05 19:14:19 +0800

    fix 005

--- Java/005 Longest Palindromic Substring.java
+++ Java/005 Longest Palindromic Substring.java
@@ -5,42 +5,38 @@
  * @author:wangzq
  * @email:wangzhenqing1008@163.com
  * @date:2015年07月02日16:12:19
+ * @update:2017-08-05 19:13:49
  * @url:https://leetcode.com/problems/longest-palindromic-substring/
  */
 
 public class Solution {
+    private int maxLen;
+    private int start;
 
     public String longestPalindrome(String s) {
         if (s == null || "".equals(s.trim())) {
             return "";
         }
+        start = 0;
+        maxLen = 1;
         char array[] = s.toCharArray();
         int len = array.length;
-        int maxLen = 1;
-        int l = 0;
-        int r = 0;
         for (int i = 0; i < len; i++) {
-            int len1 = getMaxPalindromes(i, i, len, array);
-            if (len1 > maxLen) {
-                maxLen = len1;
-                l = i - (maxLen - 1) / 2;
-                r = i + (maxLen - 1) / 2;
-            }
-            int len2 = getMaxPalindromes(i, i + 1, len, array);
-            if (len2 > maxLen) {
-                maxLen = len2;
-                l = i - maxLen / 2 + 1;
-                r = i + maxLen / 2;
-            }
+            getMaxPalindromes(array, len, i, i);
+            getMaxPalindromes(array, len, i, i + 1);
         }
-        return s.substring(l, r + 1);
+        return s.substring(start, start + maxLen);
     }
 
-    public int getMaxPalindromes(int l, int r, int len, char array[]) {
+    public void getMaxPalindromes(char array[], int len, int l, int r) {
         while (l >= 0 && r < len && array[l] == array[r]) {
             l--;
             r++;
         }
-        return r - l - 1;
+        int tempLen = r - l - 1;
+        if (tempLen > maxLen) {
+            maxLen = tempLen;
+            start = l + 1;
+        }
     }
 }
