7ceab7f1f5640241a55914fa0a0bd39b562a46a2
wzqwsrf <wzqwsrf@126.com>
2017-09-08 21:54:14 +0800

    add 526

---
+++ Java/526 Beautiful Arrangement.java
@@ -0,0 +1,33 @@
+
+// 526. Beautiful Arrangement
+
+/**
+ * @author:wangzq
+ * @email:wangzhenqing1008@163.com
+ * @date:2017-9-8 21:39:07
+ * @url:https://leetcode.com/problems/beautiful-arrangement/description/
+ */
+
+class Solution {
+    private int count;
+
+    public int countArrangement(int N) {
+        count = 0;
+        dfs(N, 1, new int[N + 1]);
+        return count;
+    }
+
+    private void dfs(int N, int currNum, int[] visited) {
+        if (currNum > N) {
+            count++;
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (visited[i] == 0 && (i % currNum == 0 || currNum % i == 0)) {
+                visited[i] = 1;
+                dfs(N, currNum + 1, visited);
+                visited[i] = 0;
+            }
+        }
+    }
+}
