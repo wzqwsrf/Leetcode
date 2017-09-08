e041f4e776f8ac2f62be49f1d1ad58c5e580a961
wzqwsrf <wzqwsrf@126.com>
2017-09-08 21:59:10 +0800

    fix 526

--- Java/526 Beautiful Arrangement.java
+++ Java/526 Beautiful Arrangement.java
@@ -13,19 +13,19 @@ class Solution {
 
     public int countArrangement(int N) {
         count = 0;
-        dfs(N, 1, new int[N + 1]);
+        dfs(N, N, new int[N + 1]);
         return count;
     }
 
     private void dfs(int N, int currNum, int[] visited) {
-        if (currNum > N) {
+        if (currNum == 0) {
             count++;
             return;
         }
         for (int i = 1; i <= N; i++) {
             if (visited[i] == 0 && (i % currNum == 0 || currNum % i == 0)) {
                 visited[i] = 1;
-                dfs(N, currNum + 1, visited);
+                dfs(N, currNum - 1, visited);
                 visited[i] = 0;
             }
         }
