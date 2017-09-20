6abe7516947264d9f916ff7bd4398f91b5ec2948
wzqwsrf <wzqwsrf@126.com>
2017-09-20 18:32:01 +0800

    add 125

--- Java/125 Valid Palindrome.java
+++ Java/125 Valid Palindrome.java
@@ -1,39 +1,45 @@
 
-// Valid Palindrome
+// 125. Valid Palindrome
 
 /**
  * @author:wangzq
  * @email:wangzhenqing1008@163.com
  * @date:2015-07-02 17:15:55
  * @url:https://leetcode.com/problems/valid-palindrome/
+ * @update:2017-9-20 18:31:20
  */
 
-public class Solution {
+class Solution {
     public boolean isPalindrome(String s) {
         if (s == null || "".equals(s.trim())) {
             return true;
         }
-        s = s.trim().replaceAll("[' ']+", "").toLowerCase();
+        s = s.toLowerCase();
         int len = s.length();
         int i = 0;
         int j = len - 1;
-        while (i <= j && i < len && j >= 0) {
-            char start = s.charAt(i);
-            char end = s.charAt(j);
-            if (!(start >= 'a' && start <= 'z') && !(start >= '0' && start <= '9')) {
+        while (i < j) {
+            if (!isAlphanumeric(s.charAt(i))) {
                 i++;
                 continue;
             }
-            if (!(end >= 'a' && end <= 'z') && !(end >= '0' && end <= '9')) {
+            if (!isAlphanumeric(s.charAt(j))) {
                 j--;
                 continue;
             }
-            if (start != end) {
+            if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
             i++;
             j--;
         }
         return true;
+    }
+
+    private boolean isAlphanumeric(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+            return true;
+        }
+        return false;
     }
 }
